81c00198462ea065e482f9ccdaed4ebb17c0c8ae
Fatima Shaikh <Fatima.Shaikh@winjit.com>
2024-07-25 16:09:10 +0530

    changes added

--- src/app/group/group-start/group-start.component.ts
+++ src/app/group/group-start/group-start.component.ts
@@ -1,10 +1,44 @@
+// import { Component } from '@angular/core';
+// import { ReactiveFormsModule, FormGroup, FormBuilder, Validators } from '@angular/forms';
+// import { GroupService } from '../group.service';
+// @Component({
+//   selector: 'app-group-start',
+//   templateUrl: './group-start.component.html',
+//   styleUrl: './group-start.component.css'
+// })
+// export class GroupStartComponent {
+//   groupForm: FormGroup;
+
+//   constructor(
+//     private fb: FormBuilder,
+//     private groupService: GroupService
+//   ) {
+//     this.groupForm = this.fb.group({
+//       name: ['', [Validators.required, Validators.minLength(2)]],
+//       description: ['']
+//     });
+//   }
+
+//   onSubmit() {
+//     if (this.groupForm.valid) {
+//       const groupData = this.groupForm.value;
+//       this.groupService.createGroup(groupData).subscribe(
+//         response => {
+//           console.log('Group created successfully:', response);
+//           // Handle success response, show success message, etc.
+//         }
+//       );
+//     }
+//   }
+// }
 import { Component } from '@angular/core';
 import { ReactiveFormsModule, FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { GroupService } from '../group.service';
+
 @Component({
   selector: 'app-group-start',
   templateUrl: './group-start.component.html',
-  styleUrl: './group-start.component.css'
+  styleUrls: ['./group-start.component.css']
 })
 export class GroupStartComponent {
   groupForm: FormGroup;
@@ -15,7 +49,8 @@ export class GroupStartComponent {
   ) {
     this.groupForm = this.fb.group({
       name: ['', [Validators.required, Validators.minLength(2)]],
-      description: ['']
+      description: [''],
+      isPublic: [false]  // Initialize the isPublic control with a default value of false
     });
   }
 
@@ -23,10 +58,14 @@ export class GroupStartComponent {
     if (this.groupForm.valid) {
       const groupData = this.groupForm.value;
       this.groupService.createGroup(groupData).subscribe(
-        response => {
+        {next: response => {
           console.log('Group created successfully:', response);
           // Handle success response, show success message, etc.
-        }
+        },
+        error: error => {
+          console.error('Error creating group:', error);
+          // Handle error response, show error message, etc.
+        }}
       );
     }
   }
